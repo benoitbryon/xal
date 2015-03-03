f774e8b64f2679864357cd914e90534469e845d5
Benoît Bryon <benoit@marmelune.net>
2015-03-03 16:50:54 +0100

    Introduced SSH setup for travis-ci.org, to allow 'ssh localhost' using fabric.

--- xal/client/fabric.py
+++ xal/client/fabric.py
@@ -7,10 +7,7 @@ from xal.client.provider import Client
 
 
 class FabricClient(Client):
-    def connect(self, user, host, port=22):
-        host = '{user}@{host}'.format(
-            user=user,
-            host=host)
+    def connect(self, host):
         fabric.api.env.hosts = [host]
         fabric.api.env.host_string = host
         return True
